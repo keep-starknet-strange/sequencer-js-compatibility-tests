9875a85b57792308118843e838e1b991f3346d0d
bexan <y.bensacq@gmail.com>
2023-11-28 18:46:03 +0100

    Tests: add cairo1 tests

--- tests/fixtures.ts
+++ tests/fixtures.ts
@@ -1,10 +1,15 @@
 const fs = require('node:fs');
 const path = require('node:path');
+require('dotenv').config();
+
 import '../mocks/Account.json';
 import '../mocks/naming_compiled.json';
 import '../mocks/starknetId_compiled.json';
 import '../mocks/cairo/helloSierra/hello.json';
 import '../mocks/cairo/helloSierra/hello.casm.json';
+import '../mocks/cairo/account/account.json';
+import '../mocks/cairo/account/account.casm.json';
+import '../mocks/cairo/complexInput/complexInput.json';
 import '../mocks/ERC20.json';
 
 import {
@@ -35,6 +40,10 @@ export const compiledStarknetId = readContract('starknetId_compiled');
 export const compiledNamingContract = readContract('naming_compiled');
 export const compiledHelloSierra = readContractSierra('cairo/helloSierra/hello');
 export const compiledHelloSierraCasm = readContractSierraCasm('cairo/helloSierra/hello');
+export const compiledC1Account = readContractSierra('cairo/account/account');
+export const compiledC1AccountCasm = readContractSierraCasm('cairo/account/account');
+export const compiledComplexSierra = readContractSierra('cairo/complexInput/complexInput');
+
 
 const describeIf = (condition: boolean) => (condition ? describe : describe.skip);
 export const describeIfDevnet = describeIf(process.env.IS_LOCALHOST_DEVNET === 'true');
